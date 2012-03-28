2e51969ff56541083f37ab565436d90a5b78a468
perdix <opitz.paul@gmx.at>
2012-03-28 16:20:32 +0200

    Erster Entwurf GUI

--- org.smartsnip/src/org/smartsnip/client/GUI.java
+++ org.smartsnip/src/org/smartsnip/client/GUI.java
@@ -1,146 +1,79 @@
 package org.smartsnip.client;
 
-import org.smartsnip.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class GUI implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
-	/**
-	 * This is the entry point method.
-	 */
+	// hier kommen alle benötigen gtw-widgets und panels rein
+	//Label label = new Label("Hello GWT !!!");
+	//Button button = new Button("Say something");
+	
+	//Create searchPanel
+	HorizontalPanel searchPanel = new HorizontalPanel();
+	TextBox searchSnippet = new TextBox();
+	ListBox searchLanguage = new ListBox();
+	Button searchButton = new Button("Search");
+	//Create leftSide
+	TabPanel leftPanel = new TabPanel();
+	VerticalPanel contentPanel0 = new VerticalPanel();
+	VerticalPanel contentPanel1 = new VerticalPanel();
+	VerticalPanel contentPanel2 = new VerticalPanel();
+	VerticalPanel contentPanel3 = new VerticalPanel();
+	VerticalPanel contentPanel4 = new VerticalPanel();
+	//Create rightSide
+	VerticalPanel rightPanel = new VerticalPanel();
+	Button cats = new Button("Categories");
+	Button tags = new Button("Tags");
+	
+	
+	
+	@Override
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		
+		//hier kann eine initialisierung vorgenommen werden
+		//z.b. stocksFlexTable.setText(0, 1, "Price");
+		
+		//hier werden die widgets in einander gebaut
+		//z.b.  addPanel.add(addStockButton);
+		
+		//Fill searchPanel
+		searchPanel.add(searchSnippet);
+		searchPanel.add(searchLanguage);
+		searchPanel.add(searchButton);
+		//Fill leftPanel
+		leftPanel.add(contentPanel0, "All");
+		leftPanel.add(contentPanel1, "Highest Rated");
+		leftPanel.add(contentPanel2, "Mostly Viewed");
+		leftPanel.add(contentPanel3, "Recently Viewd");
+		leftPanel.add(contentPanel4, "Favourites"); 
+		leftPanel.selectTab(0);
+		//Fill rightPanel
+		rightPanel.add(cats);
+		rightPanel.add(tags);
+		
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+	    //hier wird das hauptpanel oder widget zum rootpanel (div oder body) hinzugefuegt
+		//z.b.
+		//RootPanel.get().add(label);
+		//RootPanel.get().add(button);
+		
+		//Fix Panels to Divs
+		RootPanel.get("search").add(searchPanel);
+		RootPanel.get("left").add(leftPanel);
+		RootPanel.get("right").add(rightPanel);
+		
+		//hier wird der cursor gesetzt
+		//newSymbolTextBox.setFocus(true);
+		
+		
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		
 	}
 }
