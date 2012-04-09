ad7495b9be30890f9d98e46d5684f5c33e7fe14e
Felix Niederwanger <grisu42@gmail.com>
2012-04-09 21:11:17 +0200

    Singleton and Observable implemented

--- org.smartsnip/src/org/smartsnip/client/Controller.java
+++ org.smartsnip/src/org/smartsnip/client/Controller.java
@@ -1,8 +1,37 @@
 package org.smartsnip.client;
 
+import org.smartsnip.core.ISessionObserver;
+import org.smartsnip.core.Notification;
+import org.smartsnip.core.Session;
+
 public class Controller {
 	/** Singleton instance of the controller */
 	public static Controller instance;
+
+	/**
+	 * Observer of the session
+	 * 
+	 */
+	private final ISessionObserver sessionObserver = new ISessionObserver() {
+
+		@Override
+		public void notification(Session source, Notification notification) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void logout(Session source) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void login(Session source, String username) {
+			// TODO Auto-generated method stub
+
+		}
+	};
 
 	/** Static constructor initialises the singleton instance */
 	static {
@@ -13,7 +42,7 @@ public class Controller {
 	private Controller() {
 	}
 
-	// observer pattern??
 	// zuständig für kommunikation mit dem server
 	// verwaltet die GUI
+
 }
