ce77f255af534a2ab03142854713e35124e160b2
grisu48 <grisu42@gmail.com>
2012-04-19 12:25:18 +0200

    First implementation of the concrete Java Code object

---
+++ org.smartsnip/src/org/smartsnip/core/CodeJava.java
@@ -0,0 +1,32 @@
+package org.smartsnip.core;
+
+/**
+ * Concrete code object for java code
+ * 
+ */
+public class CodeJava extends Code {
+
+	public final static String language = "Java";
+
+	/**
+	 * Creates a new java code. The given code must not be null or empty.
+	 * 
+	 * @param code
+	 */
+	CodeJava(String code) {
+		super(code, language);
+	}
+
+	@Override
+	public String getFormattedHTML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String formatCode(String code) {
+		// TODO Auto-generated method stub
+		return code;
+	}
+
+}
