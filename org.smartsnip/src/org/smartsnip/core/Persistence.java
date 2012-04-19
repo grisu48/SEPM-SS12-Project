2ec73433908abd3e1517a5d98af7d25588c08476
grisu48 <grisu42@gmail.com>
2012-04-19 15:43:04 +0200

    Improved, but not yet finished

--- org.smartsnip/src/org/smartsnip/core/Persistence.java
+++ org.smartsnip/src/org/smartsnip/core/Persistence.java
@@ -17,6 +17,22 @@ public class Persistence {
 		User user2 = User.createNewUser("rubyrupert", "test", "rupert@spam.com");
 		User user3 = User.createNewUser("misterX", "test", "misterX@hidemyass.com");
 
+		Category mainCategory = Category.createCategory("Main", "Main category", null);
+		Category networkCategory = Category.createCategory("Network", "Networking snippets", null);
+		Category p2pCategory = Category.createCategory("P2P", "Networking snippets", networkCategory);
+
+		final String testCode = "// Not even hello world is found here :-/";
+		for (int i = 0; i < 10; i++) {
+
+			Snippet snippet = Snippet.createSnippet(user1, "Testsnippet" + i, "I am the test snippet #" + i,
+					new CodeJava(testCode));
+			snippet.setCategory(mainCategory);
+		}
+
+		Snippet snippet = Snippet.createSnippet(user3, "P2P", "P2P Example", new CodeJava(testCode));
+		snippet.setCategory(p2pCategory);
+
+		Comment.createComment(user2, snippet, "I like your snippet!");
 	}
 
 	/**
