3039c10af248ccc24b3d80e578b5742285a5133f
grisu48 <grisu42@gmail.com>
2012-04-20 09:35:14 +0200

    Notification manipulation methods added

--- org.smartsnip/src/org/smartsnip/core/Notification.java
+++ org.smartsnip/src/org/smartsnip/core/Notification.java
@@ -5,4 +5,56 @@ public class Notification {
 	private String message;
 	private boolean read;
 	private String time;
+
+	/**
+	 * @return the owner of the notification
+	 */
+	User getOwner() {
+		return owner;
+	}
+
+	/**
+	 * 
+	 * @return the message of the notification
+	 */
+	String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @return true if read, false if unread
+	 */
+	boolean isRead() {
+		return read;
+	}
+
+	/**
+	 * @return the sending time of the notification
+	 */
+	String getTime() {
+		return time;
+	}
+
+	/**
+	 * Marks the notification as read
+	 */
+	void markRead() {
+		read = true;
+		refreshDB();
+	}
+
+	/**
+	 * Marks the notification as unread
+	 */
+	void markUnread() {
+		read = false;
+		refreshDB();
+	}
+
+	/**
+	 * Invokes the refreshing czcle of the persistence
+	 */
+	protected synchronized void refreshDB() {
+		// TODO Implement me
+	}
 }
