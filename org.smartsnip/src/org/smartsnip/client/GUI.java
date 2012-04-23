89215a1b0b666a6568f4b9a270e813ec14d0f8ef
perdix <opitz.paul@gmx.at>
2012-04-23 17:03:41 +0200

    mieses design

--- org.smartsnip/src/org/smartsnip/client/GUI.java
+++ org.smartsnip/src/org/smartsnip/client/GUI.java
@@ -27,11 +27,11 @@ public class GUI implements EntryPoint {
 		
 		//Create the Page
 		createBasicPage();
-		//showSearchPage();
-		showImpressum();
+		showSearchPage();
+		//showImpressum();
 		//showSnipPage();
 		//showPersonalPage();
-		showLoginPopup();
+		//showLoginPopup();
 		//showRegisterPopup();
 	}
 	
@@ -51,7 +51,7 @@ public class GUI implements EntryPoint {
 		
 		// Fill searchPanel
 		SuggestBox searchSnippet = new SuggestBox();
-		Button searchButton = new Button("Search");
+		Image searchButton = new Image("images/logo_small.png");
 		searchButton.addStyleName("searchButton");
 		searchPanel.add(searchSnippet);
 		searchPanel.add(searchButton);
