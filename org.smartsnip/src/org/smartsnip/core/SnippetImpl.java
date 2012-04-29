479aa77ab032e666720150fc2e2586079e9fa709
grisu48 <grisu42@gmail.com>
2012-04-29 10:02:51 +0200

    Server-sided service implementations

---
+++ org.smartsnip/src/org/smartsnip/core/SnippetImpl.java
@@ -0,0 +1,150 @@
+package org.smartsnip.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.smartsnip.shared.IComment;
+import org.smartsnip.shared.ISnippet;
+import org.smartsnip.shared.IUser;
+import org.smartsnip.shared.NoAccessException;
+
+public class SnippetImpl extends SessionServlet implements ISnippet {
+
+	/** Associated snippet */
+	protected final Snippet snippet;
+
+	/** Default constructors initialises comment */
+	public SnippetImpl(Snippet snippet) {
+		super();
+
+		if (snippet == null)
+			throw new NullPointerException();
+		this.snippet = snippet;
+	}
+
+	@Override
+	public String getName() {
+		return snippet.getName();
+	}
+
+	@Override
+	public IUser getOwner() {
+		return session.getIUser(snippet.owner);
+	}
+
+	@Override
+	public String getDesc() {
+		return snippet.getDescription();
+	}
+
+	@Override
+	public String getLanguage() {
+		return snippet.getCode().getLanguage();
+	}
+
+	@Override
+	public int getHash() {
+		return snippet.hash;
+	}
+
+	@Override
+	public String getCodeHTML() {
+		return snippet.getCode().getFormattedHTML();
+	}
+
+	@Override
+	public List<String> getTags() {
+		List<Tag> tags = snippet.getTags();
+		ArrayList<String> result = new ArrayList<String>(tags.size());
+
+		for (Tag tag : tags) {
+			result.add(tag.name);
+		}
+
+		return result;
+	}
+
+	@Override
+	public String getCategory() {
+		return snippet.getCategory().getName();
+	}
+
+	@Override
+	public void addTag(String tag) throws NoAccessException {
+		if (!session.getPolicy().canTagSnippet(session, snippet))
+			throw new NoAccessException();
+
+		snippet.addTag(Tag.createTag(tag));
+	}
+
+	@Override
+	public void removeTag(String tag) throws NoAccessException {
+		if (!session.getPolicy().canTagSnippet(session, snippet))
+			throw new NoAccessException();
+
+		Tag tagO = Tag.getTag(tag);
+		if (tagO == null)
+			return; // Tag doesn't exists
+
+		snippet.removeTag(tagO);
+
+	}
+
+	@Override
+	public List<IComment> getComments() throws NoAccessException {
+		List<Comment> comments = snippet.getComments();
+		ArrayList<IComment> result = new ArrayList<IComment>();
+
+		for (Comment comment : comments) {
+			result.add(session.getIComment(comment));
+		}
+
+		return result;
+	}
+
+	@Override
+	public int getViewCount() {
+		return snippet.getViewcount();
+	}
+
+	@Override
+	public void increaseViewCounter() throws NoAccessException {
+		// XXX Caution: To invoke this call on the client can be a security
+		// issue
+		snippet.increaseViewCounter();
+	}
+
+	@Override
+	public IComment addComment(String comment) throws NoAccessException {
+		if (comment == null || comment.isEmpty())
+			return null;
+		if (!session.getPolicy().canComment(session))
+			throw new NoAccessException();
+
+		User user = session.getUser();
+		if (user == null)
+			throw new NoAccessException();
+		Comment result = Comment.createComment(user, snippet, comment);
+		snippet.addComment(result);
+		return session.getIComment(result);
+	}
+
+	@Override
+	public void addFavorite() {
+		session.addFavorite(snippet);
+	}
+
+	@Override
+	public void removeFavorite() {
+		session.removeFavorite(snippet);
+	}
+
+	@Override
+	public void delete() throws NoAccessException {
+		if (!session.getPolicy().canDeleteSnippet(session, snippet))
+			throw new NoAccessException();
+
+		snippet.delete();
+	}
+
+}
---
+++ org.smartsnip/src/org/smartsnip/core/UserImpl.java
@@ -0,0 +1,101 @@
+package org.smartsnip.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.smartsnip.shared.ISnippet;
+import org.smartsnip.shared.IUser;
+import org.smartsnip.shared.NoAccessException;
+
+public class UserImpl extends SessionServlet implements IUser {
+
+	/** Serialisation ID */
+	private static final long serialVersionUID = 1235424507650742693L;
+
+	/** Associated user object */
+	protected final User user;
+
+	public UserImpl(User user) {
+		super();
+
+		if (user == null)
+			throw new NullPointerException();
+		this.user = user;
+	}
+
+	@Override
+	public String getName() {
+		return user.getUsername();
+	}
+
+	@Override
+	public String getEmail() throws NoAccessException {
+		return user.getEmail();
+	}
+
+	@Override
+	public void setEmail(String newAddress) throws NoAccessException, IllegalArgumentException {
+		if (!session.getPolicy().canEditUserData(session, user))
+			throw new NoAccessException();
+		user.setEmail(newAddress);
+	}
+
+	@Override
+	public void setRealName(String newName) throws NoAccessException {
+		if (!session.getPolicy().canEditUserData(session, user))
+			throw new NoAccessException();
+
+		user.setRealName(newName);
+	}
+
+	@Override
+	public String getRealName() throws NoAccessException {
+		return user.getRealName();
+	}
+
+	@Override
+	public void logout() throws NoAccessException {
+		if (!user.equals(session.getUser()))
+			throw new NoAccessException();
+	}
+
+	@Override
+	public List<ISnippet> getSnippets() throws NoAccessException {
+		// XXX Check security privileges
+		return toISnippets(user.getMySnippets());
+	}
+
+	@Override
+	public List<ISnippet> getFavorites() throws NoAccessException {
+		// XXX Check security privileges
+		return toISnippets(user.getFavoriteSnippets());
+	}
+
+	@Override
+	public void report(String reason) {
+		if (reason == null) {
+			reason = "";
+		}
+		Session.report(user, reason);
+	}
+
+	/**
+	 * Converts a list of snippets to a list of interfaces to the corresponding
+	 * snippets
+	 * 
+	 * @param snippets
+	 * @return
+	 */
+	private List<ISnippet> toISnippets(List<Snippet> snippets) {
+		if (snippets == null)
+			return null;
+		List<ISnippet> result = new ArrayList<ISnippet>();
+
+		for (Snippet snippet : snippets) {
+			result.add(session.getISnippet(snippet));
+		}
+
+		return result;
+	}
+
+}
