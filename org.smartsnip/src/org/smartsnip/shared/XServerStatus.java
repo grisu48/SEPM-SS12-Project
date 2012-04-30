cbb2644d9ec5af94e5f8e2a368a777187a8afb6e
grisu48 <grisu42@gmail.com>
2012-04-30 19:01:56 +0200

    Server Status object

---
+++ org.smartsnip/src/org/smartsnip/shared/XServerStatus.java
@@ -0,0 +1,42 @@
+package org.smartsnip.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * This is a test class, that is used to test a message transfer from the server
+ * to the client.
+ * 
+ * Normally it should not be used in running mode ...
+ * 
+ * @author phoenix
+ * 
+ */
+public class XServerStatus implements IsSerializable {
+	private final String message;
+
+	public String servername;
+	public long totalMemory;
+	public long freeMemory;
+	public long maxMemory;
+
+	public XServerStatus() {
+		this("Hello World!");
+	}
+
+	public XServerStatus(String message) {
+		super();
+		if (message == null) {
+			message = "";
+		}
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return message;
+	}
+}
