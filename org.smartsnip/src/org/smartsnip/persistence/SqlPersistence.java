b98808924b70202c89a582481883dd681daa68f6
gerhard <theone-mail@a1.net>
2012-05-04 11:27:22 +0200

    added classes to persistence

---
+++ org.smartsnip/src/org/smartsnip/persistence/SqlPersistence.java
@@ -0,0 +1,26 @@
+/**
+ * File: SqlPersistence.java
+ * Date: 29.04.2012
+ */
+package org.smartsnip.persistence;
+
+import org.smartsnip.persistence.hibernate.SqlPersistenceImpl;
+
+/**
+ * @author littlelion
+ *
+ */
+public class SqlPersistence extends SqlPersistenceImpl {
+
+	/**
+	 * @see SqlPersistenceImpl#SqlPersistenceImpl()
+	 * @throws IllegalAccessException 
+	 * 
+	 */
+	public SqlPersistence() throws IllegalAccessException {
+		super();
+//		// XXX not implemented yet, delete next line if done
+//		throw new IllegalAccessException("not implementet yet");
+	}
+
+}
