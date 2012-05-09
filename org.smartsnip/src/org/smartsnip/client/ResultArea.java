590b8902d985d45598ec25a90676a0d6f6b028c9
perdix <opitz.paul@gmx.at>
2012-05-10 00:05:32 +0200

    resultArea

--- org.smartsnip/src/org/smartsnip/client/ResultArea.java
+++ org.smartsnip/src/org/smartsnip/client/ResultArea.java
@@ -8,8 +8,11 @@ import org.smartsnip.shared.XCategory;
 import org.smartsnip.shared.XComment;
 import org.smartsnip.shared.XSnippet;
 
+import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ScrollPanel;
 
 
 public class ResultArea extends Composite {
@@ -19,18 +22,25 @@ public class ResultArea extends Composite {
 	static final List<String> TESTTAGS = Arrays.asList("Tag1", "Tag2", "Tag3", "Tag4");
 	static final XCategory CAT = new XCategory("catname", "description", "parent", new ArrayList<String>());
 	static final XSnippet SNIPPET = new XSnippet("owner", 123,"title", "description", CAT, TESTTAGS, new ArrayList<XComment>(),"code", "codeHTML", "language", "license", 4);
-	static final List<XSnippet> TESTLIST = Arrays.asList(SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET);
+	static final List<XSnippet> TESTLIST = Arrays.asList(SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET, SNIPPET);
 	
 	
 	public ResultArea() {
+		
+		ScrollPanel myPanel = new ScrollPanel();
+		
 		ShortSnipCell myShortSnipCell = new ShortSnipCell();
 		
 		myCellList = new CellList<XSnippet>(myShortSnipCell);
 		myCellList.setRowData(0, TESTLIST);
+		//Some special Features
+		
 
-	
+		myCellList.setRowCount(TESTLIST.size(), true);
 
-		initWidget(myCellList);
+		myPanel.setHeight("600px");
+		myPanel.add(myCellList);
+		initWidget(myPanel);
 	    // Give the overall composite a style name.
 	    setStyleName("resultArea");
 	}
