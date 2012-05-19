b3e7a1fcd76abb8be52e4c13723da094f49efccc
grisu48 <grisu42@gmail.com>
2012-05-19 10:37:28 +0200

    First runnable version

--- org.smartsnip/src/org/smartsnip/core/Logging.java
+++ org.smartsnip/src/org/smartsnip/core/Logging.java
@@ -3,44 +3,85 @@ package org.smartsnip.core;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Logging {
 
 	public static PrintStream err = new PrintStream(new OutputStream() {
 
 		@Override
-		public void write(int arg0) throws IOException {
-			// TODO Auto-generated method stub
-
+		public void write(int b) throws IOException {
+			System.err.write(b);
+		}
+	});
+	public static PrintStream out = new PrintStream(new OutputStream() {
+		
+		@Override
+		public void write(int b) throws IOException {
+			System.out.write(b);
 		}
 	});
 
 	public static void printWarning(String warning) {
-
+		if (warning == null || warning.isEmpty()) return;
+		out.println(getTimeHeader() + "[WARN] " + warning);
 	}
 
 	public static void printWarning(Throwable throwable) {
-
+		if (throwable == null) return;
+		printWarning("Exception " + throwable.getMessage());
 	}
 
 	public static void printWarning(String message, Throwable throwable) {
+		if (message == null || message.isEmpty()) {
+			printWarning(throwable);
+			return;
+		}
+		if (throwable == null) {
+			printWarning(message);
+			return;
+		}
+		printWarning(message + "(Exception: " + throwable.getMessage() + ")");
 
 	}
 
-	public static void printError(String error) {
-
+	public static void printError(String message) {
+		if (message == null || message.isEmpty()) return;
+		err.println(getTimeHeader() + "[ERR] " + message);
 	}
 
 	public static void printError(Throwable throwable) {
-
+		if (throwable == null) return;
+		printError("Exception " + throwable.getMessage());
 	}
 
 	public static void printError(String message, Throwable throwable) {
+		if (message == null || message.isEmpty()) {
+			printError(throwable);
+			return;
+		}
+		if (throwable == null) {
+			printError(message);
+			return;
+		}
+		printError(message + "(Exception: " + throwable.getMessage() + ")");
 
 	}
 
 	public static void printInfo(String info) {
-
+		if (info == null || info.isEmpty()) return;
+		
+		out.println("[INFO] " + info);
 	}
 
+	
+	/**
+	 * @return Gets a formatted time header for the messages
+	 */
+	private static String getTimeHeader() {
+		SimpleDateFormat simpleDateFormat =
+			        new SimpleDateFormat("yyyy.MM.dd");
+		return "[" + simpleDateFormat.format(new Date()) + "] ";
+	}
 }
