f167ca5ae6cda9c2000ab210d6f70c728d0645be
perdix <opitz.paul@gmx.at>
2012-05-19 16:25:38 +0200

    css label and anchor

--- org.smartsnip/src/org/smartsnip/client/SortArea.java
+++ org.smartsnip/src/org/smartsnip/client/SortArea.java
@@ -1,14 +1,12 @@
 package org.smartsnip.client;
 
 
-import org.smartsnip.shared.ISnippet;
-
 
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+
 
 public class SortArea extends Composite {
 	
@@ -19,11 +17,9 @@ public class SortArea extends Composite {
 		myPanel = new FlowPanel();
 		title = new Label("Sorting");
 		myPanel.add(title);
-		myPanel.add(new Button("All"));
 		myPanel.add(new Button("Highest Rated"));
 		myPanel.add(new Button("Mostly Viewed"));
-		myPanel.add(new Button("Recently Viewed"));
-		myPanel.add(new Button("My Favourites"));
+
 	
 		initWidget(myPanel);
 	    // Give the overall composite a style name.
