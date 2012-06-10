07a95f6e053d8f035437745214c63f471df9fda6
grisu48 <grisu42@gmail.com>
2012-06-10 10:28:58 +0200

    Anti-Rote-Knödel Update

---
+++ org.smartsnip/src/org/smartsnip/core/JavaFormatter.java
@@ -0,0 +1,45 @@
+package org.smartsnip.core;
+
+public class JavaFormatter extends HTMLFormatter {
+
+	private String code = "";
+	private int currentPos = 0;
+
+	private StringBuffer result = null;
+
+	/** Java keywords */
+	private final String[] keywords = new String[] { "abstract", "continue",
+			"for", "new", "switch", "assert", "default", "goto", "package",
+			"synchronized", "boolean", "do", "if", "private", "this", "break",
+			"double", "implements", "protected", "throw", "byte", "else",
+			"import", "public", "throws", "case", "enum", "instanceof",
+			"return", "transient", "catch", "extends", "int", "short", "try",
+			"char", "final", "interface", "static", "void", "class", "finally",
+			"long", "volatile", "const", "float", "native", "super" };
+
+	JavaFormatter() {
+	}
+
+	@Override
+	public synchronized String format(String code) {
+		if (code == null || code.isEmpty())
+			return "";
+
+		this.code = code;
+		currentPos = 0;
+		result = new StringBuffer();
+		result.append("<code>");
+
+		// Format keywords
+		for (String keyword : keywords) {
+			code = code.replace(keyword, "<b>" + keyword + "</b>");
+		}
+
+		// TODO Improve formatter!
+
+		result.append(code);
+		result.append("</code>");
+
+		return result.toString();
+	}
+}
