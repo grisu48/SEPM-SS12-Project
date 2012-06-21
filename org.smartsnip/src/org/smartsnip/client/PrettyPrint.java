8d99e6fb4c0f411c3e90b7ac3d216ecf4df369e0
grisu48 <grisu42@gmail.com>
2012-06-21 15:58:50 +0200

    Added documentation

--- org.smartsnip/src/org/smartsnip/client/PrettyPrint.java
+++ org.smartsnip/src/org/smartsnip/client/PrettyPrint.java
@@ -4,12 +4,32 @@ import org.smartsnip.shared.XCode;
 
 import com.google.gwt.user.client.ui.HTMLPanel;
 
+/**
+ * This panel implements the PrettyPrint highlighter. The result is a panel,
+ * that displays a given code with syntax highlighting.
+ * 
+ * @author Felix Niederwanger
+ * 
+ */
+
 public class PrettyPrint extends HTMLPanel {
 
+	/**
+	 * Initialise the pretty print highlighter with given code in html format
+	 * 
+	 * @param codeHTML
+	 *            codehtml to be interpreted
+	 */
 	public PrettyPrint(String codeHTML) {
 		super(codeHTML);
 	}
 
+	/**
+	 * Initialise the pretty print highlighter with given {@link XCode} object
+	 * 
+	 * @param code
+	 *            code object to be highlightes
+	 */
 	public PrettyPrint(XCode code) {
 		this(code.codeHTML);
 	}
